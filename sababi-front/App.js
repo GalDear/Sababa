a20730cea99876cc55a5387b7140c8f9a6fbec3e
Daniel George <daniel96geo@gmail.com>
2022-07-23 18:38:20 +0300

    Bottom nav

--- sababi-front/App.js
+++ sababi-front/App.js
@@ -1,80 +1,17 @@
-// import React from "react";
-// import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
-// export default function App() {
-
-//   const handlePress = () => console.log("dick");
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Text onPress={handlePress}> Open up App.js to start working on your app!</Text>
-//     </SafeAreaView>
-    
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// import React from "react";
-// import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-// import {NavigationContainer} from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// //import { createStackNavigator } from '@react-navigation/stack';
-// // import { registerRootComponent } from 'expo';
-// // import App from './App';
-
-// const Stack = createNativeStackNavigator();
-
-
-// function ScreenA(){
-//   return(
-//     <View>
-//       <Text>
-//         Screen A
-//       </Text>
-//     </View>
-//   )
-// }
-
-// export default function App(){
-//   return(
-//     <NavigationContainer>
-//       {<Stack.Navigator>
-//         <Stack.Screen
-//         name = "ScreenA"
-//         componnet = {ScreenA}
-//         />
-//       </Stack.Navigator>
-//     }
-
-//     </NavigationContainer>
-    
-//   )
-// }
-
-// registerRootComponent(App);
-
-
-
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './Views/MainScreen';
+import {Footer} from './Componnats/Footer';
+
 
 function HomeScreen2({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen2</Text>
       <Button title="home" onPress={() => navigation.navigate('Home')}/>
+      
     </View>
   );
 }
@@ -82,13 +19,29 @@ function HomeScreen2({navigation}) {
 const Stack = createNativeStackNavigator();
 
 function App() {
+  
+  const [data, setData] = React.useState('');
+  const childToParent = (childdata) => {
+    setData(childdata);
+  }
+
+  console.log(data);
   return (
+    
     <NavigationContainer>
+      <View style={{
+        height: '95%'
+      }}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Home2" component={HomeScreen2} />
       </Stack.Navigator>
+      </View>
+      <View style={{
+        height: '5%'
+      }}><Footer childToParent={childToParent}></Footer></View>
     </NavigationContainer>
+    
   );
 }
 
