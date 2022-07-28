975a711e0c3dd172b69bf69f45c66b15a9228853
Gal Bachar <gal.bachar@cymotive.com>
2022-07-28 10:47:04 +0300

    upload image works

--- server/api/media.py
+++ server/api/media.py
@@ -2,7 +2,7 @@ from flask import make_response,jsonify,request
 def image():
     if(request.method == "POST"):
         bytesOfImage = request.get_data()
-        with open('../files/image.jpeg', 'wb') as out:
+        with open('files/image.jpeg', 'wb+') as out:
             out.write(bytesOfImage)
         return "Image read"
 
@@ -10,6 +10,6 @@ def image():
 def video():
     if(request.method == "POST"):
         bytesOfVideo = request.get_data()
-        with open('../files/video.mp4', 'wb') as out:
+        with open('files/video.mp4', 'wb+') as out:
             out.write(bytesOfVideo)
         return "Video read"
