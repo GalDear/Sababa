21fbd22973e1f131b4970f7782a3a6ff553e9b7b
Eden Meshulam <Edenm@zeusint.com>
2022-07-31 12:09:48 +0300

    Dockerfile - added regex pip intsall. db_models - added parse method for user type and gender, added autoincrement for id in user db model. registration - validate email and password -> check if a user with the same email already exist -> add user.

--- server/api/registration.py
+++ server/api/registration.py
@@ -1,5 +1,7 @@
+from operator import truediv
 from db_models import Users,db
 import json
+import re
 from flask import make_response,jsonify,request
 
 
@@ -10,17 +12,32 @@ def user_registration():
         data = json.loads(request.data)
         print(data)
         try:
-            new_user = Users(password=data['password'], full_name=data['full_name'], phone_number=data['phone_number'], email=data['email'],country=data['country'],created_at=datetime.today(),user_type=data['user_type'],description=data['description'],foundation=data['foundation'],gender=data['gender'],skills=data['skills'])
-            print(new_user)
-            db.session.add(new_user)
-            db.session.commit()
-            response = make_response({'success':True})
-            print(new_user)
+            if(validateEmail(data['email']) and validatePassword(data['password'])):
+                new_user = Users(password=data['password'], full_name=data['full_name'], phone_number=data['phone_number'], email=data['email'],country=data['country'],created_at=datetime.today(),user_type=data['user_type'],description=data['description'],foundation=data['foundation'],gender=data['gender'],skills=data['skills'])
+                print(new_user)
+                db.session.add(new_user)
+                db.session.commit()
+                response = make_response({'success':True})
+                print(new_user)
+            else:
+                response = make_response({'success':False, 'error':"Invalid username or password"})
+                response.status_code = 400
         except Exception as e:
             print(e)
-            response.status_code = 500
             response = make_response({'success':False})
+            response.status_code = 500
+        
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
         
+
+
+emailRegex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+def validateEmail(email):
+    return re.fullmatch(emailRegex, email) # return boolean value, indicate if email is in correct format
+ 
+
+passwordRegex = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}$")
+def validatePassword(password):
+    return re.search(passwordRegex, password)
 
