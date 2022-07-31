301f4f2ba04a7243e4572beac6e8258fbfd3d30c
Eden Meshulam <Edenm@zeusint.com>
2022-07-31 12:13:16 +0300

    Dockerfile - added regex pip intsall. db_models - added parse method for user type and gender, added autoincrement for id in user db model. registration - validate email and password -> check if a user with the same email already exist -> add user.

--- server/api/registration.py
+++ server/api/registration.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from operator import truediv
-from db_models import Users,db
+from db_models import Users,db,Genders,UserTypes
 import json
 import re
 from flask import make_response,jsonify,request
+
+
 
 
 def user_registration():
@@ -13,17 +16,23 @@ def user_registration():
         print(data)
         try:
             if(validateEmail(data['email']) and validatePassword(data['password'])):
-                new_user = Users(password=data['password'], full_name=data['full_name'], phone_number=data['phone_number'], email=data['email'],country=data['country'],created_at=datetime.today(),user_type=data['user_type'],description=data['description'],foundation=data['foundation'],gender=data['gender'],skills=data['skills'])
-                print(new_user)
-                db.session.add(new_user)
-                db.session.commit()
-                response = make_response({'success':True})
-                print(new_user)
+                if Users.query.filter_by(email=data['email']).first() is None:
+                    new_user = Users(password=data['password'], full_name=data['full_name'], phone_number=data['phone_number'], email=data['email'],country=data['country'],created_at=datetime.today(),user_type=UserTypes.Parse(data['user_type']),description=data['description'],foundation=data['foundation'],gender=Genders.Parse(data['gender']),skills=data['skills'])
+                    print(new_user)
+                    db.session.add(new_user)
+                    db.session.commit()
+                    response = make_response({'success':True})
+                    print(new_user)
+                    
+                else:
+                    response = make_response({'success':False, 'error':"Email is already taken"})
+                    response.status_code = 406
+                    
             else:
                 response = make_response({'success':False, 'error':"Invalid username or password"})
                 response.status_code = 400
         except Exception as e:
-            print(e)
+            print(f"EXEPTION: {e}")
             response = make_response({'success':False})
             response.status_code = 500
         
